c6eeba11a99e9c637ff327404c4ddd35ca42691d
Aqilaaaaaaa <haqilaan@gmail.com>
2022-08-31 12:13:38 +0700

    burger button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,32 @@
 import styles from "../styles/nav.module.css";
 import Link from 'next/link'
+import React, { useState } from "react";
+
+
 
 export default function Navbar (){
+  const [isMobile, setIsMobile] = useState(false);
+  
+
     return (
       <div className={styles.container}>
         <div className={styles.content}>
           <div className={styles.nama}>
-          Hunger App
+          Hunger App 
           </div>
         </div> 
-        <ul className={styles.menu}>
-            <li className={styles.item}><Link className="" href="#Home">Home</Link></li>
-            <li className={styles.item}><Link className="" href="#Fav">Favorite</Link></li>
-            <li className={styles.item}><Link className="" href="#About">About Us</Link></li>
-          </ul>                             
+        <ul className={!isMobile ? styles.menuLink : styles.menu}>
+          <li className={styles.item}><Link className="" href="#">Home</Link></li>
+          <li className={styles.item}><Link className="" href="#">Favorite</Link></li>
+          <li className={styles.item}><Link className="" href="#">About Us</Link></li>
+        </ul>  
+        <button className={styles.burgerBtn} onClick={() => setIsMobile(false)}>
+          {isMobile ? (
+            <i className="fas fa-times"></i>
+          ) : (
+            <i className="fas fa-bars"></i>
+          )}
+        </button>                           
       </div>
     )
 }
